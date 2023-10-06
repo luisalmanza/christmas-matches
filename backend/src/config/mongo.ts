997d7b9edb12fc5d11205cae71f77b3da3731234
Luis Almanza <luis.almanzacastro@gmail.com>
2023-10-06 12:34:13 -0700

    Add DB_URI_TEST for unit testing

--- backend/src/config/mongo.ts
+++ backend/src/config/mongo.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import { connect } from "mongoose";
 
+const NODE_ENV = process.env.NODE_ENV;
+
 async function dbConnect(): Promise<void> {
-    const DB_URI = <string>process.env.DB_URI;
+    const DB_URI = (NODE_ENV === "test") ? <string>process.env.DB_URI_TEST : <string>process.env.DB_URI;
     await connect(DB_URI);
 }
 
